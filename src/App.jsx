d349cf77723e8d03d71971ba124d1bf5e656c537
mumuttt <peerayaforwork.f@gmail.com>
2023-09-18 10:18:37 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Profile from './peeraya/Profile'
+function App() {
+  return (
+    <>
+    <Profile/>
+      
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/peeraya/Profile.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import App from '../App';
+
+// const userprofile =[
+//     {
+//         name: "John",
+//         lastnames: "Doe",
+//         img: "",
+//     }
+
+// ]
+// import 
+const ProfileComponent = () => {
+    const [name, setName] = useState('John'); 
+    const [lastname, setLastname] = useState('Doe'); 
+    const [aboutme,setAboutme] = useState()
+    const showProfile = async (name,lastname) => {
+        
+    }
+
+    return (
+        <>
+            <div className="bg-sky-900">
+                <h2 className='text-neutral-50 '>My Profile</h2>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <p> {name}  {lastname}</p>
+            </div>
+            <div >
+                <p> About me  {aboutme} </p>
+            </div>
+        </>
+    )
+}
+
+export default ProfileComponent;
+
+
